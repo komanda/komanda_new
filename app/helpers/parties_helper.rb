7f6e08912aabe27148de5fdbe3b3a34a0d4091dc
Nikolay Smirnov <nsmirnov.y@gmail.com>
2012-12-21 18:35:34 -0800

    use thumbs instead of full images

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -1,5 +1,4 @@
 module PartiesHelper  
-  
   def render_current_rating(rating)
     html = ""
     rating.times { html += "<i class='icon-star'></i>" }
@@ -29,4 +28,16 @@ module PartiesHelper
     end
   end
   
+  def get_thumb_url(url)
+    # http://imageshack.us/a/img717/8971/image7xd.jpg - URL
+    # http://imageshack.us/scaled/thumbx2/717/image7xd.jpg - thumb
+    if (url.include?("/scaled/"))
+      tokens = url.split("/")
+      tokens[4] = "thumbx2"
+      return tokens.join("/")
+    else  
+      tokens = url.split("/")
+      return "http://imageshack.us/scaled/thumbx2/#{tokens[4].slice(3, tokens[4].length)}/#{tokens[6]}"
+    end
+  end
 end
